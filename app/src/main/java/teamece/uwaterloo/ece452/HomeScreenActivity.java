9a91499190b8d5edf9cd92fae79cd870dcfc42a4
Chengming Lu <cluster626@gmail.com>
2019-06-03 22:48:34 -0400

    Part 1 of skeleton code

---
+++ app/src/main/java/teamece/uwaterloo/ece452/HomeScreenActivity.java
@@ -0,0 +1,19 @@
+package teamece.uwaterloo.ece452;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class HomeScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+
+    public void startNewGame(View v) {
+        Intent newGameActivity = new Intent(this, GameActivity.class);
+        startActivity(newGameActivity);
+    }
+}
