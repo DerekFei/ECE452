e4f1c77dc578c3d2bf138d0d2613d0aff7bb66d9
ZHEYUAN ZHANG <z464zhan@uwaterloo.ca>
2019-06-18 22:57:45 -0400

    Implement geese and lines

---
+++ app/src/main/java/teamece/uwaterloo/ece452/Goose.java
@@ -0,0 +1,41 @@
+package teamece.uwaterloo.ece452;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+public class Goose implements GameObject {
+    private Rect hitBox;
+    private boolean left;
+    private boolean leftLane;
+    private int width;
+    private int height;
+
+    private void generateHitBox(){
+        hitBox.set((int)((left?0:width*3)+(leftLane?width*0.25:width*1.75)), height*5,
+                (int)((left?0:width*3)+(leftLane?width*1.25:width*2.75)),
+                height*6);
+    }
+
+    public Goose(boolean l, int windowWidth, int windowHeight){
+        left=l;
+        leftLane=l;
+        width = windowWidth/6;
+        height = windowHeight/6;
+        hitBox = new Rect((int)((left?0:width*3)+(leftLane?width*0.25:width*1.75)), height*5,
+                (int)((left?0:width*3)+(leftLane?width*1.25:width*2.75)),
+                height*6);
+    }
+
+    public void draw(Canvas canvas){
+        Paint p = new Paint();
+        p.setColor(Color.BLACK);
+        canvas.drawRect(hitBox, p);
+    }
+
+    public void update(){
+        leftLane = !leftLane;
+        generateHitBox();
+    }
+}
