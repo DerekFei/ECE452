3db68e50ad746e8665c4290ae2d7dc2c0ea5c08e
Derek Fei <dukefitch@hotmail.com>
2019-06-18 23:20:12 -0400

    backend code

---
+++ backend/geece/src/main/java/com/example/geece/GeeceApplication.java
@@ -0,0 +1,13 @@
+package com.example.geece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeeceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeeceApplication.class, args);
+    }
+
+}
