482ea95dd1836a2aedbbf11418122f5ff0e4ffee
ZHEYUAN ZHANG <z464zhan@uwaterloo.ca>
2019-06-19 15:52:15 -0400

    Implement falling objects (with bug)

--- app/src/main/java/teamece/uwaterloo/ece452/FallingDevice.java
+++ app/src/main/java/teamece/uwaterloo/ece452/FallingDevice.java
@@ -1,6 +1,53 @@
 package teamece.uwaterloo.ece452;
 
-import GameObject;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+import java.io.Console;
 
 public abstract class FallingDevice implements GameObject {
+    private boolean left;
+    private boolean leftLane;
+    private int width;
+    private int height;
+    private int altitude;
+    private boolean start;
+    private Rect hitBox;
+
+    public FallingDevice(boolean left, boolean leftlane, int windowWidth, int windowHeight){
+        left = left;
+        leftLane = leftlane;
+        width = windowWidth / 6;
+        height = windowHeight / 8;
+        altitude = - height / 2;
+        start = false;
+        hitBox = new Rect((int)((left ? 0 : width * 3) + (leftLane ? width * 0.25 : width * 1.75)), 0,
+                (int)((left ? 0 : width * 3) + (leftLane ? width * 1.25 : width * 2.75)), 0);
+    }
+
+    public void update(){
+        if(start) {
+            altitude = altitude + 20;
+            generateHitBox();
+            if(altitude>9*height)
+                start = false;
+        }
+    }
+
+    public void initialize(){
+        start = true;
+        altitude = - height / 2;
+        generateHitBox();
+    }
+
+    private void generateHitBox(){
+        hitBox.set((int)((left ? 0 : width * 3) + (leftLane ? width * 0.25 : width * 1.75)), altitude - height / 2,
+                (int)((left ? 0 : width * 3) + (leftLane ? width * 1.25 : width * 2.75)), altitude + height / 2);
+    }
+
+    public void draw(Canvas canvas, Paint paint){
+        canvas.drawRect(hitBox, paint);
+    }
 }
--- app/src/main/java/teamece/uwaterloo/ece452/FallingLED.java
+++ app/src/main/java/teamece/uwaterloo/ece452/FallingLED.java
@@ -1,7 +1,17 @@
 package teamece.uwaterloo.ece452;
 
-import FallingDevice;
-import GameObject;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 
 public class FallingLED extends FallingDevice implements GameObject {
+    public FallingLED(boolean left, boolean leftlane, int windowWidth, int windowHeight){
+        super(left, leftlane, windowWidth, windowHeight);
+    }
+
+    public void draw(Canvas canvas){
+        Paint p = new Paint();
+        p.setColor(Color.YELLOW);
+        super.draw(canvas, p);
+    }
 }
