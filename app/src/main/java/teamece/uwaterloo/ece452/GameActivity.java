65e791203ee2099d8634024887431264a0bc314d
TONYYYYYYYYYY <453740573@qq.com>
2019-06-19 22:18:51 -0400

    high top bars

--- app/src/main/java/teamece/uwaterloo/ece452/GameActivity.java
+++ app/src/main/java/teamece/uwaterloo/ece452/GameActivity.java
@@ -2,6 +2,7 @@ package teamece.uwaterloo.ece452;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -10,8 +11,10 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(new GameScene(this));
     }
 }
--- app/src/main/java/teamece/uwaterloo/ece452/LeaderBoardActivity.java
+++ app/src/main/java/teamece/uwaterloo/ece452/LeaderBoardActivity.java
@@ -3,6 +3,9 @@ package teamece.uwaterloo.ece452;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import java.util.List;
@@ -19,6 +22,10 @@ public class LeaderBoardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_leaderboard_screen);
 
         leaderBoardResult = findViewById(R.id.leader_board_result);
