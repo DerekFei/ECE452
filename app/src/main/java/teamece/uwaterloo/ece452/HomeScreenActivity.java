c46d13b8f3d64d9de3d2d483035c7fc26b49980a
Chengming Lu <cluster626@gmail.com>
2019-07-09 20:38:30 -0400

    Before migrating to androidx

--- app/src/main/java/teamece/uwaterloo/ece452/HomeScreenActivity.java
+++ app/src/main/java/teamece/uwaterloo/ece452/HomeScreenActivity.java
@@ -35,6 +35,37 @@ public class HomeScreenActivity extends AppCompatActivity {
 
     }
 
+    private void signInSilently() {
+        GoogleSignInOptions signInOptions = GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN;
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (GoogleSignIn.hasPermissions(account, signInOptions.getScopeArray())) {
+            // Already signed in.
+            // The signed in account is stored in the 'account' variable.
+            GoogleSignInAccount signedInAccount = account;
+        } else {
+            // Haven't been signed-in before. Try the silent sign-in first.
+            GoogleSignInClient signInClient = GoogleSignIn.getClient(this, signInOptions);
+            signInClient
+                    .silentSignIn()
+                    .addOnCompleteListener(
+                            this,
+                            new OnCompleteListener<GoogleSignInAccount>() {
+                                @Override
+                                public void onComplete(@NonNull Task<GoogleSignInAccount> task) {
+                                    if (task.isSuccessful()) {
+                                        // The signed in account is stored in the task's result.
+                                        GoogleSignInAccount signedInAccount = task.getResult();
+                                    } else {
+                                        // Player will need to sign-in explicitly using via UI.
+                                        // See [sign-in best practices](http://developers.google.com/games/services/checklist) for guidance on how and when to implement Interactive Sign-in,
+                                        // and [Performing Interactive Sign-in](http://developers.google.com/games/services/android/signin#performing_interactive_sign-in) for details on how to implement
+                                        // Interactive Sign-in.
+                                    }
+                                }
+                            });
+        }
+    }
+
     @Override
     protected void onRestart() {
         super.onRestart();
@@ -49,6 +80,12 @@ public class HomeScreenActivity extends AppCompatActivity {
                 mp.setLooping(true);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        signInSilently();
     }
 
     public void startNewGame(View v) {
