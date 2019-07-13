063f5649e5b85d68faaa63a9b6365c45e94ae228
ZHEYUAN ZHANG <z464zhan@uwaterloo.ca>
2019-07-13 00:20:30 -0400

    Implement Help screens
    BUGS:
    1. Background videos are not played, leaving a white background
    2. Images not showing in scrollviews for unknown reason

---
+++ app/src/main/java/teamece/uwaterloo/ece452/HelpActivity.java
@@ -0,0 +1,73 @@
+package teamece.uwaterloo.ece452;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class HelpActivity extends FragmentActivity {
+    private static final int NUM_PAGES = 4;
+
+    private ViewPager2 mPager;
+
+    private FragmentStateAdapter pagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+        mPager = (ViewPager2) findViewById(R.id.viewpager2);
+        mPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+        });
+        mPager.setOrientation(ViewPager2.ORIENTATION_VERTICAL);
+        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(pagerAdapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() == 0) {
+            super.onBackPressed();
+        } else {
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStateAdapter {
+        ArrayList<Fragment> l = new ArrayList<Fragment>();
+
+        public ScreenSlidePagerAdapter(FragmentManager fa) {
+            super(fa);
+            l.add(new HelpFragment1());
+            l.add(new HelpFragment2());
+            l.add(new HelpFragment3());
+            l.add(new HelpFragment4());
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return l.get(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return NUM_PAGES;
+        }
+    }
+}
---
+++ app/src/main/java/teamece/uwaterloo/ece452/HelpFragment4.java
@@ -0,0 +1,19 @@
+package teamece.uwaterloo.ece452;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class HelpFragment4 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.help_fragment_4, container, false);
+
+        return rootView;
+    }
+}
